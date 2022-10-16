86567db7940fed14acc3975915dc689bcb1e8bce
TomekSkrzypek <tomeksone@gmail.com>
2022-10-17 00:28:53 +0200

    clean up and some code

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import {Outlet, NavLink} from "react-router-dom";
 import {PageHeader} from '../PageHeader/PageHeader';
 import {ScrollPage} from "../ScrollPage/ScrollPage";
+import './Layout.scss'
 
 export function Layout() {
     return (
-        <div>
-            <PageHeader />
-            <ScrollPage />
-            <Outlet />
+        <div className="Layout">
+
+            <PageHeader/>
+
+            <ScrollPage/>
+
+            <main className="Layout__main">
+                <Outlet/>
+            </main>
+
         </div>
     )
 }
