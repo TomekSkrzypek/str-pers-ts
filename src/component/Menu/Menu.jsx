3a909587694ef7de9e6c1a6f69c35278024bd2e6
TomekSkrzypek <tomeksone@gmail.com>
2022-10-18 21:14:06 +0200

    2114

--- src/component/Menu/Menu.jsx
+++ src/component/Menu/Menu.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import {Navigate, Link} from "react-router-dom";
 import './Menu.scss'
+
 // import {Blog} from "../../pages/Blog/Blog.jsx";
 
-export function Menu() {
-    return (
-        <div>
-            <ul className="Menu">
-                <li><Link className="Menu__item" to="/">Strona główna</Link></li>
-                <li><Link className="Menu__item" to="/blog">Blog</Link></li>
-                <li><Link className="Menu__item" to="/mail">Mail</Link></li>
+export function Menu({toggle, isOpen}) {
+    // ${({isOpen}) => (isOpen ? Menu.style.display='flex' : Menu.style.display='none')}
 
+    return (
+        <>
+            {isOpen && <div>
+            <ul className="Menu">
+                <li><Link className="Menu__item" to="/" onClick={toggle}>Strona główna</Link></li>
+                <li><Link className="Menu__item" to="/blog" onClick={toggle}>Blog</Link></li>
+                <li><Link className="Menu__item" to="/mail" onClick={toggle}>Mail</Link></li>
             </ul>
-        </div>
+        </div>}
+        </>
     )
 }
--- src/component/PageHeader/PageHeader.jsx
+++ src/component/PageHeader/PageHeader.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Logo} from "../Logo/Logo";
 import {Menu} from "../Menu/Menu";
 import './PageHeader.scss';
+import {HiOutlineX} from 'react-icons/hi';
+import {HiOutlineMenu} from 'react-icons/hi';
 
 
 export function PageHeader() {
+   const [isOpen, setIsOpen] = useState(false);
+    console.log("isOpen state = " + isOpen);
+
+   const toggle = () => {
+       setIsOpen(!isOpen)
+       console.log("toggle ! is open state=" + isOpen)
+   }
+
     return (
-        <header className="PageHeader">
-            <Logo/>
-            <Menu/>
-        </header>
+        <div className="PageHeaderAndIcons">
+            <header className="PageHeader">
+                <Logo/>
+                <Menu isOpen={isOpen} toggle={toggle} />
+            </header>
+            <HiOutlineMenu onClick={toggle} className="PageHeader__MenuIcon"/>
+            <HiOutlineX onClick={toggle} className="PageHeader__CloseIcon"/>
+        </div>
     )
 }
+
