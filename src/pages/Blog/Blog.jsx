f740dc94d2ca9e32e258ae8acd031cec4e355b5a
TomekSkrzypek <tomeksone@gmail.com>
2022-10-25 17:09:30 +0200

    nowe

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -4,7 +4,9 @@ import './Blog.scss';
 export function Blog() {
     return (
         <div className="sectionBlog__container">
-            <section className="sectionBlog__one">
+
+
+            <section className="section sectionBlog__one">
 
 
                 <h1>BLOG adpivnoklnnolnlc</h1>
@@ -17,7 +19,8 @@ export function Blog() {
                     facilis id illo ipsa laboriosam magni minima modi molestias placeat possimus praesentium quasi, quod
                     quos repellat rerum sequi sit sunt tempore temporibus velit voluptatibus voluptatum! Accusantium
                     aspernatur consequatur cumque earum magnam nostrum officia pariatur possimus quibusdam vitae.</p>
-
+            </section>
+            <section className="section sectionBlog__two">
                 <h2>Drugi Artykuł</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi id laborum laudantium maxime
@@ -32,7 +35,8 @@ export function Blog() {
                     deleniti doloremque dolores error, est ex id in inventore libero magni maiores nam nisi pariatur
                     reiciendis sapiente tempora vitae. Ab aut dolorem doloribus, et labore nisi nobis quis sequi sit
                     temporibus veniam voluptatem. Magni, officia.</p>
-
+            </section>
+            <section className="section sectionBlog__three">
                 <h2>Trzeci artykuł</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab, alias aliquid amet animi atque
@@ -50,7 +54,8 @@ export function Blog() {
                     saepe similique tempora totam veniam veritatis voluptatibus. Aliquid architecto deleniti deserunt
                     nesciunt quibusdam. Aliquid aspernatur at, culpa cumque deleniti eligendi id mollitia nemo nesciunt
                     numquam obcaecati quidem rem tempore voluptas voluptatem? Necessitatibus.</p>
-
+            </section>
+            <section className="section sectionBlog__four">
                 <h2>Czwraty artykuł</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi consectetur debitis eveniet
@@ -76,8 +81,55 @@ export function Blog() {
                     consequatur deserunt doloribus eaque laboriosam laborum magnam molestias, mollitia neque nisi
                     nostrum numquam perspiciatis provident quidem repudiandae sit voluptates. A alias amet animi beatae
                     blanditiis deleniti, dolorem doloremque earum expedita harum illo, molestiae molestias numquam
-                    officiis quo recusandae sapiente similique sit suscipit voluptatem!
-                KONIEC</p>
+                    officiis quo recusandae sapiente similique sit suscipit voluptatem! Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Accusantium error expedita explicabo optio possimus. Culpa deleniti
+                    ducimus fugiat nobis numquam odit tenetur? Aspernatur itaque numquam officiis tempore! Delectus
+                    dolore expedita minus modi praesentium quo quod sapiente vel velit voluptatum? Blanditiis culpa eius
+                    explicabo facilis ipsum molestias quia? A accusantium animi architecto blanditiis culpa cupiditate
+                    deleniti dolor eos et eveniet explicabo in ipsam iste labore laudantium maxime molestias nam,
+                    necessitatibus nemo nostrum obcaecati perferendis quia, quidem reiciendis rem suscipit unde ut vel
+                    vitae voluptatibus! Consequatur magni quisquam recusandae tempore. Architecto autem corporis enim
+                    facere, ipsum maiores, maxime molestias nihil omnis praesentium, quidem recusandae voluptatem.
+                    Aperiam architecto asperiores, culpa dolorem doloribus eius enim esse est et facere facilis fuga
+                    fugiat harum in laborum molestiae nemo neque nulla numquam obcaecati omnis quod rem repudiandae, sit
+                    veritatis! Assumenda dolor, dolore eligendi eum fuga iste labore pariatur qui quibusdam rem sed
+                    suscipit voluptatibus? Blanditiis earum exercitationem nesciunt numquam reiciendis repudiandae.
+                    Consectetur delectus dolor doloremque eum nisi suscipit veniam. Atque dolore iste libero optio
+                    perspiciatis porro? Autem commodi dicta, dolores esse excepturi, impedit inventore ipsam itaque
+                    iusto obcaecati, quae quia sint vero! Accusantium architecto at deserunt, doloremque dolorum et
+                    excepturi laudantium libero nobis possimus quasi qui ratione recusandae saepe sunt ullam unde. Ab
+                    corporis dicta ea earum ex fugit id iste, maiores pariatur perspiciatis placeat porro repellendus
+                    saepe veritatis vitae voluptas, voluptatibus! Adipisci consectetur consequatur cupiditate debitis,
+                    dignissimos explicabo facere harum impedit laudantium nostrum nulla omnis perspiciatis, quae quaerat
+                    quam rem repellat sed sint tempora tenetur? Accusantium corporis eius fuga fugit laboriosam maxime
+                    omnis, provident veniam! Aperiam dolor facilis iste, necessitatibus odit similique sit tenetur. A
+                    autem blanditiis error in ipsa magni minus molestias quisquam voluptas. Dolorem ea est eveniet harum
+                    labore, molestiae nam quasi quidem recusandae totam. Adipisci alias, consectetur dolores dolorum
+                    natus nulla quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad beatae consequuntur
+                    culpa delectus deserunt dicta dolor doloremque dolores dolorum ducimus ea enim error exercitationem
+                    explicabo fuga, hic illo impedit ipsa iste itaque iusto libero magnam molestias necessitatibus neque
+                    omnis perspiciatis, quaerat quibusdam quod similique tempora tempore temporibus tenetur ullam ut
+                    veritatis vitae? Aspernatur, beatae dolor dolore dolores doloribus eius, esse eum exercitationem
+                    fugit inventore iusto libero maiores minus molestiae placeat quaerat quas sapiente tempore totam ut!
+                    Cum cupiditate, debitis dignissimos dolore eaque ex excepturi facere iusto mollitia nostrum pariatur
+                    provident reiciendis rem rerum saepe sapiente sunt tempora vel velit voluptate! Cum dolorem eum iure
+                    nulla ratione! A ad adipisci aliquam, aspernatur at atque corporis cum deserunt dicta dignissimos et
+                    eveniet expedita explicabo facilis fugit harum incidunt iure nihil non nulla obcaecati odit officiis
+                    optio pariatur perspiciatis placeat quam qui recusandae reiciendis repellendus reprehenderit sunt
+                    suscipit unde vel vero vitae voluptate. Dicta dolores dolorum, eligendi error facilis fuga in ipsa
+                    maiores necessitatibus odit officiis perferendis quibusdam sequi sunt vero. Accusantium asperiores
+                    blanditiis cupiditate, et facere ipsam magnam magni minima, numquam optio placeat quaerat quos
+                    reiciendis sapiente tenetur! Atque consequuntur eligendi hic maxime quaerat quos velit! Accusamus
+                    aliquam assumenda atque autem consectetur, corporis debitis deleniti doloremque dolorum ducimus eius
+                    eligendi enim ex explicabo fugiat harum impedit incidunt laboriosam magni maiores minima nesciunt
+                    odit omnis pariatur quasi reprehenderit repudiandae sed similique temporibus vero! Illum nobis
+                    quidem repudiandae sunt. Atque, ipsa quisquam? Aliquam earum eius excepturi in incidunt perferendis
+                    placeat repudiandae. Asperiores aut, commodi culpa error illo laboriosam nemo nesciunt officia rem,
+                    rerum vero voluptate. A ad dolores magnam tempora! Aliquam architecto delectus deserunt
+                    exercitationem, hic iure magnam modi mollitia nihil, nobis odio odit placeat rerum soluta ullam,
+                    veritatis voluptas! Accusamus beatae doloribus, impedit, iusto nemo nesciunt possimus quisquam
+                    recusandae sequi sit tempore vero.
+                    KONIEC</p>
             </section>
         </div>
     )
