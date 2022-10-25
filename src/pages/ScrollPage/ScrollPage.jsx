f66399f0e93ab33440d5bb6101610a15fa2efd46
TomekSkrzypek <tomeksone@gmail.com>
2022-10-25 19:34:00 +0200

    gda

--- src/pages/ScrollPage/ScrollPage.jsx
+++ src/pages/ScrollPage/ScrollPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import './ScrollPage.scss';
 // import supabase from "../../config/supabaseClient";
-// import IMG_1623 from '../../assets/IMG_1623.jpg'
+import IMG_1623 from '../../../public/IMG_1623.jpg'
 
 export function ScrollPage() {
     // console.log("supa aasdbase" + supabase)
     return (
         // <div className="section__uber-container">
             <div className="section__container">
-                <section className="section__one">
+                <section >
+                    <img src={IMG_1623} className="section__one"/>
                     <h1></h1>
                 </section>
                 <section className="section__two">
